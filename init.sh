0252afb30751a3d421e851eacf4813c04f951636
tulenivnebe <empty@mail.here>
2017-05-02 22:07:43 +0300

    added django

--- init.sh
+++ init.sh
@@ -4,7 +4,9 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
 sudo service nginx restart
 
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/hello.wsgi /etc/gunicorn.d/hello.wsgi
+sudo ln -sf /home/box/web/etc/qa.wsgi /etc/gunicorn.d/qa.wsgi
+
 sudo service gunicorn start
+
 
